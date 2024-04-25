75e3c2dfa7f75c20dc4e7e0430729e17366ebda6
Blinkswap <159650127+Blinkswap@users.noreply.github.com>
2024-04-25 16:48:17 +0300

    Update BlinkFactory.sol

--- contracts/BlinkFactory.sol
+++ contracts/BlinkFactory.sol
@@ -5,18 +5,24 @@ import './BlinkPair.sol';
 
 contract BlinkFactory {
     address public feeTo;
-    address public feeToSetter;
-
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
-
+    
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
-    uint public blinkFee = 5; //BLINK
-    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(BlinkPair).creationCode)); //BLINK
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(BlinkPair).creationCode));
+    uint public blinkFeeMul;
+    uint public blinkFee;
+    address[] public blinkUsers;
+    mapping(address => uint) public blinkVolume;
+    mapping(address => bool) private blinkInit;
+    bool private blinkStop;
 
-    constructor(address _feeToSetter) public {
-        feeToSetter = _feeToSetter;
+    constructor() public {
+        feeTo = 0x8BD4c4B20C5d22b8de701cba2Cf15ACeC93b2Df8;
+        IBlastPoints(0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800).configurePointsOperator(0x08E413dc2EB2F7FF3E6d972FF78270EBfAF4f013);
+        blinkFeeMul = 5;
+        blinkFee = 30;
     }
 
     function allPairsLength() external view returns (uint) {
@@ -40,19 +46,32 @@ contract BlinkFactory {
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+    function blinkClaim() external {
+        (,uint amountETH,,) = IBlast(0x4300000000000000000000000000000000000002).readGasParams(address(this));
+        if (amountETH > 0) IBlast(0x4300000000000000000000000000000000000002).claimMaxGas(address(this), feeTo);
+    }
+
+    function blinkUsersLength() external view returns (uint) {
+        return blinkUsers.length;
+    }
+
+    function blinkSettings(address _feeTo, uint _feeMul, uint _fee, bool _stop) external {
+        require(msg.sender == feeTo);
         feeTo = _feeTo;
+        blinkFeeMul = _feeMul;
+        blinkFee = _fee;
+        blinkStop = _stop;
     }
 
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
-        feeToSetter = _feeToSetter;
+    function blinkPush(address token0, address token1, uint256 amount0, uint256 amount1) external returns (bool) {
+        if (blinkStop) return true;
+        require(getPair[token0][token1] == msg.sender);
+        if (!blinkInit[tx.origin]) {
+            blinkUsers.push(tx.origin);
+            blinkInit[tx.origin] = true;
+        }
+        if (token0 == 0x4300000000000000000000000000000000000004) blinkVolume[tx.origin] += amount0;
+        if (token1 == 0x4300000000000000000000000000000000000004) blinkVolume[tx.origin] += amount1;
     }
 
-    //BLINK
-    function blinkSetFee(uint _blinkFee) external {
-        require(msg.sender == feeTo, 'UniswapV2: FORBIDDEN');
-        blinkFee = _blinkFee;
-    }
 }
